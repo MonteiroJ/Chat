4739b7bc15ab6140692d8cd42d16741b592c6229
MonteiroJ <josemonteiro.baggio@gmail.com>
2018-07-11 16:22:48 +0200

    Modification du port

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    fs = require('fs');
+    const PORT = process.env.PORT || 5000
+
+// Chargement de la page index.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    }); 
+
+    // Dès que l'utilisateur se déconnecte, on prévient les autres utilisateurs
+    socket.on('disconnect', function(){
+        console.log(socket.pseudo + ' disconnected');
+        socket.broadcast.emit('deconnexion_client', socket.pseudo);
+      });
+});
+
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
